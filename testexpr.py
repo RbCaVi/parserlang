739494a08a31f3daf22cdbcbca9de1d615fbdf25
RbCaViTerminal <robert@robertvail.info>
2024-03-10 21:04:41 -0400

    use treeexpr

--- testexpr.py
+++ testexpr.py
@@ -1,19 +1,25 @@
 from parser import printresults
 from parsers import parserify
-from evalexpr import evaluate
+from evalexpr import evaluate,treeexpr
+
+def treeexpr2(expr):
+	return '\n\n'+treeexpr(expr)+'\n'
 
 ep=parserify(evaluate)
 
-printresults(ep,'')
-printresults(ep,'1')
-printresults(ep,'1+2')
-printresults(ep,'(1)')
-printresults(ep,'a(1)')
-printresults(ep,'a(1,2)')
-printresults(ep,'[]')
-printresults(ep,'[1]')
-printresults(ep,'[1,2]')
-printresults(ep,'a(1,)')
-printresults(ep,'a(1,2,)')
-printresults(ep,'[1,]')
-printresults(ep,'[1,2,]')
+printresults(ep,'',formatter=treeexpr2)
+printresults(ep,'1',formatter=treeexpr2)
+printresults(ep,'1+2',formatter=treeexpr2)
+printresults(ep,'(1)',formatter=treeexpr2)
+printresults(ep,'a[1]',formatter=treeexpr2)
+printresults(ep,'a[1][2]',formatter=treeexpr2)
+printresults(ep,'(a)[2]',formatter=treeexpr2)
+printresults(ep,'a(1)',formatter=treeexpr2)
+printresults(ep,'a(1,2)',formatter=treeexpr2)
+printresults(ep,'[]',formatter=treeexpr2)
+printresults(ep,'[1]',formatter=treeexpr2)
+printresults(ep,'[1,2]',formatter=treeexpr2)
+printresults(ep,'a(1,)',formatter=treeexpr2)
+printresults(ep,'a(1,2,)',formatter=treeexpr2)
+printresults(ep,'[1,]',formatter=treeexpr2)
+printresults(ep,'[1,2,]',formatter=treeexpr2)
