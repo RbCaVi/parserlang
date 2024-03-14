0557a2288325898d6e177ed2249395db84f6274a
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2024-03-14 15:05:40 +0000

    Add jv

---
+++ pv/util.h
@@ -0,0 +1,20 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+const void *_jq_memmem(const void *haystack, size_t haystacklen,
+                       const void *needle, size_t needlelen);
+
+#ifndef MIN
+#define MIN(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a < _b ? _a : _b; })
+#endif
+#ifndef MAX
+#define MAX(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+   __typeof__ (b) _b = (b); \
+   _a > _b ? _a : _b; })
+#endif
+
+#endif /* UTIL_H */
