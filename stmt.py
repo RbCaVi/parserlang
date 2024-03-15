30c310b161ca1c414ac70cdb02e351434184db9a
RbCaViTerminal <robert@robertvail.info>
2024-03-15 12:39:44 -0400

    concatstrip to strip all arguments

--- stmt.py
+++ stmt.py
@@ -4,13 +4,16 @@ from expr import evaluate,EXPR,ops,getSym,SYM
 
 expr=parserify(evaluate)
 
+def concatstrip(*ps):
+	return concat(*map(ps))
+
 @parser
 def sym(s):
 	data,s=getSym(s)
 	if data:
 		yield [SYM,data],s
 
-@transform(concat(expr,strip(strs('=',*[op+'=' for op in ops])),expr))
+@transform(concatstrip(expr,strs('=',*[op+'=' for op in ops]),expr))
 def assign(data):
 	e1,aop,e2=data
 	return [EXPR,aop,e1,e2]
@@ -20,7 +23,7 @@ typep=strip(alternate()) # no values
 TYPE='TYPE'
 anytype=[TYPE,'any']
 
-@transform(concat(strs('def'),optional(typep),errorafter(strip(sym)),strip(strs('=')),expr))
+@transform(concatstrip(strs('def'),optional(typep),errorafter(sym),strs('='),expr))
 def declare(data):
 	_,typ,var,__,e=data
 	if not typ:
