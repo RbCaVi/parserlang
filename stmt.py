152572c0f1d91b18ca5369536b107997f010abd2
RbCaViTerminal <robert@robertvail.info>
2024-03-14 21:50:30 -0400

    rename ep to expr

--- stmt.py
+++ stmt.py
@@ -1,9 +1,9 @@
 from parsers import parserify,concat,alternate,strs,strip,transform
 from expr import evaluate,EXPR,ops
 
-ep=parserify(evaluate)
+expr=parserify(evaluate)
 
-@transform(concat(ep,strip(strs('=',*[op+'=' for op in ops])),ep))
+@transform(concat(expr,strip(strs('=',*[op+'=' for op in ops])),expr))
 def assign(data):
 	e1,aop,e2=data
 	return [EXPR,aop,e1,e2]
