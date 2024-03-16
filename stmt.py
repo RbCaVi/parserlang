5b282c17eb1ece5b30815361321ab5357806dcaa
RbCaViTerminal <robert@robertvail.info>
2024-03-16 09:52:41 -0400

    add if statement and change type of statement to STMT

--- stmt.py
+++ stmt.py
@@ -4,6 +4,9 @@ from expr import evaluate,EXPR,ops,getSym,SYM
 
 expr=parserify(evaluate)
 
+def stmtwrap(s): # "backreference"
+	return stmt(s)
+
 def concatstrip(*ps):
 	return concat(*map(ps))
 
@@ -16,11 +19,12 @@ def sym(s):
 @transform(concatstrip(expr,strs('=',*[op+'=' for op in ops]),expr))
 def assign(data):
 	e1,aop,e2=data
-	return [EXPR,aop,e1,e2]
+	return [STMT,aop,e1,e2]
 
 typep=strip(alternate()) # no values
 
 TYPE='TYPE'
+STMT='STMT'
 anytype=[TYPE,'any']
 
 @transform(concatstrip(strs('def'),optional(typep),errorafter(sym),strs('='),expr))
@@ -28,7 +32,12 @@ def declare(data):
 	_,typ,var,__,e=data
 	if not typ:
 		typ=anytype
-	return [EXPR,'def',typ,var,e]
+	return [STMT,'def',typ,var,e]
+
+@transform(concatstrip(strs('if'),ep,strs('then'),stmtwrap,strs('end')))
+def ifstmt(data):
+	_,cond,__,s,___=data
+	return [STMT,'if',typ,var,e]
 
 @transform(alternate(assign,declare))
 def stmt(data):
