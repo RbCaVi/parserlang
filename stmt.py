dc05cdcf413004815bd08778d8af21268a30ca00
RbCaViTerminal <robert@robertvail.info>
2024-03-27 21:52:28 -0400

    function and expression statement fix

--- stmt.py
+++ stmt.py
@@ -1,5 +1,5 @@
 from parser import parser
-from parsers import parserify,concat,alternate,strs,strip,transform,optional,errornone,errorafter
+from parsers import parserify,concat,alternate,strs,strip,transform,optional,errornone,errorafter,star,noneerror
 from expr import evaluate,EXPR,ops,getSym,SYM
 
 expr=parserify(evaluate)
@@ -27,6 +27,7 @@ TYPE='TYPE'
 STMT='STMT'
 ARG='ARG'
 SIG='SIG'
+FN='FN'
 anytype=[TYPE,'any']
 
 @transform(concatstrip(strs('def'),optional(typep),errorafter(sym),strs('='),expr))
@@ -46,6 +47,7 @@ def commasep(p):
 			return []
 		d,others,_=data
 		return [d,*others]
+	return commasep
 
 @transform(concatstrip(optional(typep),optional(sym)))
 def arg(data):
@@ -57,11 +59,15 @@ def funcsig(data):
 	name,_,args,_=data
 	return [SIG,name,*args]
 
-@transform(concatstrip(strs('fn'),funcsig,star(stmtwrap),strs('end')))
+@transform(concatstrip(strs('fn'),funcsig,star(noneerror(stmtwrap)),strs('end')))
 def func(data):
-	_,sig,_,stmts,_=data
+	_,sig,stmts,_=data
+	return [STMT,'func',sig[1],[EXPR,'sig',*sig[2:]],*stmts]
 
+@transform(expr)
+def exprstmt(data):
+	return [STMT,'expr',data]
 
-@transform(alternate(ifstmt,declare,assign,expr))
+@transform(alternate(func,ifstmt,declare,assign,exprstmt))
 def stmt(data):
 	return data[1]
