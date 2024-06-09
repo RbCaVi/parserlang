d9b058309f63332d0155d4ecaa3aac0ee593e105
RbCaViTerminal <robert@robertvail.info>
2024-06-09 15:07:58 -0400

    add pl_stack.c

---
+++ src/pl/pl_stack.c
@@ -0,0 +1,29 @@
+#include "pl/pl_stack.h"
+#include <assert.h>
+
+pv pl_stack_get(pl_stack stack,int idx) {
+  assert(idx != 0);
+  if (idx > 0) {
+  	assert(locals + idx < top);
+  	return stack.cells->cells[locals + idx].value->value; // the longest chain of properties i have ever written
+  } else {
+  	assert(idx < top);
+  	return stack.cells->cells[top - idx].value->value; // the longest chain of properties i have ever written
+  }
+}
+
+pl_stack pl_stack_pop(pl_stack stack) {
+	assert()
+}
+
+pl_stack pl_stack_set(pl_stack stack,pv val,int idx) {
+	// set makes a new stack always
+}
+
+pl_stack pl_stack_push(pl_stack stack,pv val) {
+	// push makes a new stack always
+}
+
+pl_stack pl_stack_push_ref(pl_stack stack,int idx) {
+	// push makes a new stack always
+}
--- src/pl/pl_stack.h
+++ src/pl/pl_stack.h
@@ -28,19 +28,27 @@ struct pl_stack_cell {
   };
 };
 
-typedef struct {
+struct pl_stack_cells {
   size_t refcount;
-  struct pl_stack_cell* cells;
+  struct pl_stack_cell[] cells;
+};
+
+typedef struct {
+  struct pl_stack_cells* cells;
   size_t size; // the number of stack cells
   size_t top; // the number of filled cells
-  size_t locals; // the stack index locals start at
+  size_t locals; // the stack index locals start at // the location of retinfo
 } pl_stack;
 
 // stack indexes are positive up from locals or negative down from stack top
 // 0 is the retinfo (not used)
 
-pv pl_stack_get(pl_stack,size_t);
-pv pl_stack_pop(pl_stack);
-pl_stack pl_stack_set(pl_stack,pv,size_t);
+pv pl_stack_get(pl_stack,int);
+#define pl_stack_top(stack) pl_stack_get(stack,-1)
+pl_stack pl_stack_pop(pl_stack);
+pl_stack pl_stack_set(pl_stack,pv,int);
 pl_stack pl_stack_push(pl_stack,pv);
-pl_stack pl_stack_push_ref(pl_stack,size_t);
+pl_stack pl_stack_push_ref(pl_stack,int);
+pl_stack pl_stack_add_frame(pl_stack);
+pl_stack pl_stack_ref(pl_stack);
+void pl_stack_decref(pl_stack);
