beac83e4ae15f9c0f8a082a709e64efe45e2986b
RbCaViTerminal <robert@robertvail.info>
2024-06-12 18:51:45 -0400

    add inc_size() macro

---
+++ src/pl/util_pl.h
@@ -0,0 +1,19 @@
+// save the top in idx
+// increments the top
+// if top is out of bounds, reallocate for newsize elements
+// initial is the size of anything before the array data
+// idx is the name of a variable to store the index of the new element
+// array is the array to add an element to
+// top is the number of elements in the array
+// size is the number of allocated slots
+// assumes top, size, and array can be assigned to
+#define inc_size(idx,array,top,initial,size,newsize) \
+(idx) = (top); \
+(top)++; \
+if ((top) >= (size)) { \
+  (size) = (newsize); \
+  (array) = realloc((array),(initial) + (size) * sizeof(typeof(*(array)))); \
+  if ((array) == NULL) { \
+    abort(); \
+  } \
+}
