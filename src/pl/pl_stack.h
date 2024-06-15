91301d382f7d3bafe8145469c012c515b194a1a9
RbCaViTerminal <robert@robertvail.info>
2024-06-14 20:26:26 -0400

    pl_stack struct member changes

--- src/pl/pl_stack.h
+++ src/pl/pl_stack.h
@@ -6,7 +6,7 @@
  */
 
 struct pl_stack_cell_data {
-  int refcount;
+  int refcount; // number of stack cells using this value (references)
   pv value;
 };
 
@@ -20,7 +20,6 @@ enum pl_stack_cell_type {
 };
 
 struct pl_stack_cell {
-  size_t refcount;
   enum pl_stack_cell_type type;
   union {
     struct pl_stack_cell_data *value;
@@ -28,9 +27,13 @@ struct pl_stack_cell {
   };
 };
 
-struct pl_stack_cells {
+struct pl_stack_cells_refcnt {
   size_t refcount; // the number of stacks using these cells
   size_t size; // the number of stack cells
+};
+
+struct pl_stack_cells {
+  struct pl_stack_cells_refcnt refcount;
   struct pl_stack_cell cells[]; // the stack data
 };
 
