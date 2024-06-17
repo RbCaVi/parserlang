1bda2cf695af5ceed347429fb7c380e7c59100bc
RbCaViTerminal <robert@robertvail.info>
2024-06-17 11:24:23 -0400

    remove references and correcter pv refcount

--- src/pl/pl_stack.c
+++ src/pl/pl_stack.c
@@ -2,12 +2,8 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pl/util_pl.h"
-
-struct pl_stack_cell_data {
-  int refcount; // number of stack cells using this value (references)
-  pv value;
-};
 
 struct pl_retinfo {
   size_t locals;
@@ -21,7 +17,7 @@ enum pl_stack_cell_type {
 struct pl_stack_cell {
   enum pl_stack_cell_type type;
   union {
-    struct pl_stack_cell_data *value;
+    pv value;
     struct pl_retinfo ret;
   };
 };
@@ -38,44 +34,42 @@ struct pl_stack_cells {
 
 pv pl_stack_get(pl_stack stack,int idx) {
   assert(idx != 0);
+  pv out;
   if (idx > 0) {
   	assert(stack.locals + (size_t)idx < stack.top);
-  	return stack.cells->cells[stack.locals + (size_t)idx].value->value; // the longest chain of properties i have ever written
+  	out = stack.cells->cells[stack.locals + (size_t)idx].value; // the longest chain of properties i have ever written
   } else {
   	assert((size_t)(-idx) < stack.top);
-  	return stack.cells->cells[stack.top - (size_t)(-idx)].value->value; // the longest chain of properties i have ever written
+  	out = stack.cells->cells[stack.top - (size_t)(-idx)].value; // the longest chain of properties i have ever written
   }
+  return pv_ref(out);
 }
 
 pl_stack pl_stack_pop(pl_stack stack) {
 	assert(stack.top > stack.locals + 1); // +1 for the retinfo at the bottom of a frame
 	stack.top--;
+	pv_unref(stack.cells->cells[stack.top].value); // delete the stack top
 	return stack;
-}
-
-void replace_pv_pointer(pv *to_replace, pv replacement) {
-	pv_ref(replacement);
-	pv_unref(*to_replace);
-	*to_replace = replacement;
 }
 
 static pl_stack duplicate_stack(pl_stack stack) {
 	pl_stack newstack = stack;
 	// duplicate the stack cells
-	size_t size = sizeof(struct pl_stack_cells_refcnt) + stack.cells->refcount.size * sizeof(typeof(stack.cells.cells));
+	size_t size = sizeof(struct pl_stack_cells_refcnt) + stack.cells->refcount.size * sizeof(typeof(stack.cells->cells[0]));
 	newstack.cells = malloc(size);
   if (newstack.cells == NULL) {
     abort();
   }
-	// have to copy the references too
 	// copy the cells
 	memcpy(newstack.cells,stack.cells,size);
-	
+
+	return newstack;
 }
 
 pl_stack pl_stack_set(pl_stack stack,pv val,int idx) {
 	// set makes a new stack always
 	if (stack.cells->refcount.refcount > 1) {
+		pl_stack_decref(stack);
 		stack = duplicate_stack(stack);
 	}
   assert(idx != 0);
@@ -87,7 +81,8 @@ pl_stack pl_stack_set(pl_stack stack,pv val,int idx) {
   	assert((size_t)(-idx) < stack.top);
   	i = stack.top - (size_t)(-idx);
   }
-  replace_pv_pointer(&(stack.cells->cells[i].value->value), val);
+  pv_unref(stack.cells->cells[i].value); // delete the previous value
+  stack.cells->cells[i].value = val;
 
   return stack;
 }
@@ -96,26 +91,12 @@ pl_stack pl_stack_push(pl_stack stack,pv val) {
 	size_t idx;
 	// push makes a new stack always
 	if (stack.cells->refcount.refcount > 1) {
+		pl_stack_decref(stack);
 		stack = duplicate_stack(stack);
 	}
 	inc_size(idx,stack.cells,stack.top,sizeof(struct pl_stack_cells_refcnt),stack.cells->refcount.size,(size_t)((float)stack.cells->refcount.size * 1.5f));
   // initialize the new cell
-	stack.cells->cells[idx].value->refcount = 1;
-	stack.cells->cells[idx].value->value = pv_ref(val);
-
-	return stack;
-}
-
-pl_stack pl_stack_push_ref(pl_stack stack,int sidx) {
-	size_t idx;
-	// push makes a new stack always
-	if (stack.cells->refcount.refcount > 1) {
-		stack = duplicate_stack(stack);
-	}
-	inc_size(idx,stack.cells,stack.top,sizeof(struct pl_stack_cells_refcnt),stack.cells->refcount.size,(size_t)((float)stack.cells->refcount.size * 1.5f));
-  // initialize the new cell
-	stack.cells->cells[idx].value = stack.cells->cells[sidx].value;
-	stack.cells->cells[idx].value->refcount++;
+	stack.cells->cells[idx].value = val;
 
 	return stack;
 }
