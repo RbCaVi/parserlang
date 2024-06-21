56ce769bf9a93cf878140db3884f1aba1cf941d9
RbCaViTerminal <robert@robertvail.info>
2024-06-21 09:30:44 -0400

    split the dump code to its own file

---
+++ src/pl/pl_dump.c
@@ -0,0 +1,73 @@
+#include "pl/pl_dump.h"
+#include "pl/util_pl.h"
+
+static void print_prefix(pl_dump_prefix parts) {
+	printf(".");
+	for (size_t i = 0; i < parts.count; i++) {
+		pl_dump_prefix_part part = parts.data->parts[i];
+		printf("/");
+		switch (part.type) {
+		case STR:
+			printf("\"%s\"",part.str);
+			break;
+		case IDX:
+			printf("%i",part.idx);
+			break;
+		case KEY:
+			printf("%s",part.str);
+			break;
+		}
+	}
+	printf(": ");
+}
+
+void pl_dump_pv(pv val) {
+	pl_dump_prefix parts;
+	parts.data = checked_malloc(sizeof(size_t) + sizeof(pl_dump_prefix_part) * 4);
+	parts.data->size = 4;
+	parts.count = 0;
+	pl_dump_pv_prefixed(val, parts);
+}
+
+void pl_dump_pv_prefixed(pv val, pl_dump_prefix parts) {
+	print_prefix(parts);
+	size_t idx;
+	switch (pv_get_kind(val)) {
+  case PV_KIND_INVALID:
+  	printf("ERROR OBJECT\n");
+  	break;
+  case PV_KIND_NULL:
+  	printf("NULL\n");
+  	break;
+  case PV_KIND_FALSE:
+  	printf("FALSE\n");
+  	break;
+  case PV_KIND_TRUE:
+  	printf("TRUE\n");
+  	break;
+  case PV_KIND_NUMBER:
+  	printf("%f\n",pv_number_value(val));
+  	break;
+  case PV_KIND_STRING:
+  	printf("\"%s\"\n",pv_string_value(val));
+  	break;
+  case PV_KIND_ARRAY:
+  	printf("[]\n");
+		inc_size(idx,parts.data,parts.count,sizeof(size_t),parts.data->size,(size_t)((float)parts.data->size * 1.5f));
+		pv_array_foreach(val, i, v) {
+			parts.data->parts[idx].type = IDX;
+			parts.data->parts[idx].idx = i;
+			pl_dump_pv_prefixed(v, parts);
+  	}
+  	break;
+  case PV_KIND_OBJECT:
+  	printf("{}\n");
+		inc_size(idx,parts.data,parts.count,sizeof(size_t),parts.data->size,(size_t)((float)parts.data->size * 1.5f));
+		pv_object_foreach(val, k, v) {
+			parts.data->parts[idx].type = STR;
+			parts.data->parts[idx].str = pv_string_value(k);
+			pl_dump_pv_prefixed(v, parts);
+  	}
+  	break;
+	}
+}
---
+++ src/pl/pl_dump.h
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+#include "pv/pv.h"
+
+typedef struct {
+	enum {
+		STR,IDX,KEY
+	} type;
+	union {
+		const char *str; // STR and KEY
+		int idx;
+	};
+} pl_dump_prefix_part;
+
+typedef struct {
+	struct {
+		size_t size;
+	  pl_dump_prefix_part parts[];
+	} *data;
+	size_t count;
+} pl_dump_prefix;
+
+void pl_dump_pv(pv val);
+void pl_dump_pv_prefixed(pv val, pl_dump_prefix parts);
