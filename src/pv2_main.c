e1ad923e1bbe10e997e4976c1afe67dec740d415
RbCaViTerminal <robert@robertvail.info>
2024-09-06 15:03:41 -0400

    test the name of kind 0x00

--- src/pv2_main.c
+++ src/pv2_main.c
@@ -8,5 +8,6 @@ int main(int argc, char **argv) {
 	pv val = pv_number(15);
 	char *s = pv_to_string(val);
 	double num = pv_number_value(val);
-	printf("%f, %s", num, s);
+	printf("%f, %s\n", num, s);
+	printf("%s\n", pv_kind_name(0));
 }
