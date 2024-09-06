6bded85b2dddf05e8419f24e6ab5f9482c660077
RbCaViTerminal <robert@robertvail.info>
2024-09-06 17:04:02 -0400

    add a to string for null true and false

--- src/pv2/pv_singletons.c
+++ src/pv2/pv_singletons.c
@@ -1,12 +1,14 @@
 #include "pv_singletons.h"
+#include "pv_to_string.h"
 
 #include <assert.h>
 #include <stddef.h>
+#include <string.h>
 
 pv_kind null_kind;
 pv_kind bool_kind;
 
-// this is a great idea - casting int to pointer is definitely more illegal
+// this is a great idea - casting int to pointer is definitely worse
 
 static struct pv_refcnt *cast_int_to_pointer(int val) {
 	// illegal
@@ -30,11 +32,24 @@ static int cast_pointer_to_int(struct pv_refcnt *ptr) {
 	return u.val;
 }
 
+static char *pv_singleton_to_string(pv val) {
+	if (pv_get_kind(val) == null_kind) {
+		return strdup("NULL");
+	}
+	if (pv_bool_value(val)) {
+		return strdup("TRUE");
+	} else {
+		return strdup("FALSE");
+	}
+}
+
 void pv_singletons_install() {
 	// be nice if there was a static assert but
 	assert(sizeof(int) <= sizeof(struct pv_refcnt*));
 	pv_register_kind(&null_kind, "null", NULL);
 	pv_register_kind(&bool_kind, "bool", NULL);
+	pv_register_to_string(null_kind, pv_singleton_to_string);
+	pv_register_to_string(bool_kind, pv_singleton_to_string);
 }
 
 pv pv_null() {
@@ -59,5 +74,5 @@ pv pv_bool(int b) {
 
 int pv_bool_value(pv val) {
 	assert(val.kind == bool_kind);
-	return (cast_pointer_to_int(val.data) > 0) ? 1 : -1;
+	return (cast_pointer_to_int(val.data) > 0) ? 1 : 0;
 }
