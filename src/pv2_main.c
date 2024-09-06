fdb7ddfaf35353f9ed571354c81474f73dea89ca
RbCaViTerminal <robert@robertvail.info>
2024-09-06 13:02:53 -0400

    a test main for my pv

---
+++ src/pv2_main.c
@@ -0,0 +1,12 @@
+#include "pv.h"
+#include "pv_number.h"
+#include "pv_to_string.h"
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	pv val = pv_number(15);
+	char *s = pv_to_string(val);
+	double num = pv_number_value(val);
+	printf("%f, %s", num, s);
+}
