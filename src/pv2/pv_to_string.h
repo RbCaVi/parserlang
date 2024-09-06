4485f11404ad79f8385e3d83f429f5c19e96bbbf
RbCaViTerminal <robert@robertvail.info>
2024-09-06 14:18:09 -0400

    remove argument names because uhhhhhh

--- src/pv2/pv_to_string.h
+++ src/pv2/pv_to_string.h
@@ -3,6 +3,7 @@
 
 #include "pv.h"
 
-void pv_register_to_string(pv_kind kind, char *(*f)(pv));
-char *pv_to_string(pv val);
+void pv_register_to_string(pv_kind, char *(*)(pv));
+char *pv_to_string(pv);
+
 #endif
