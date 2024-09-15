c0c19e65c3aca03964ed461d900180ac0413cd60
RbCaViTerminal <robert@robertvail.info>
2024-09-14 21:30:23 -0400

    "avoid array constant re allocation"

--- src/pv2/pv_array.h
+++ src/pv2/pv_array.h
@@ -24,17 +24,17 @@ pv pv_array_concat(pv, pv);
            (x = pv_array_get(pv_ref(a), i), 1) : 0;                  \
          i++)
 
-#define PV_ARRAY_1(e) (pv_array_append(pv_array(),e))
-#define PV_ARRAY_2(e1,e2) (pv_array_append(PV_ARRAY_1(e1),e2))
-#define PV_ARRAY_3(e1,e2,e3) (pv_array_append(PV_ARRAY_2(e1,e2),e3))
-#define PV_ARRAY_4(e1,e2,e3,e4) (pv_array_append(PV_ARRAY_3(e1,e2,e3),e4))
-#define PV_ARRAY_5(e1,e2,e3,e4,e5) (pv_array_append(PV_ARRAY_4(e1,e2,e3,e4),e5))
-#define PV_ARRAY_6(e1,e2,e3,e4,e5,e6) (pv_array_append(PV_ARRAY_5(e1,e2,e3,e4,e5),e6))
-#define PV_ARRAY_7(e1,e2,e3,e4,e5,e6,e7) (pv_array_append(PV_ARRAY_6(e1,e2,e3,e4,e5,e6),e7))
-#define PV_ARRAY_8(e1,e2,e3,e4,e5,e6,e7,e8) (pv_array_append(PV_ARRAY_7(e1,e2,e3,e4,e5,e6,e7),e8))
-#define PV_ARRAY_9(e1,e2,e3,e4,e5,e6,e7,e8,e9) (pv_array_append(PV_ARRAY_8(e1,e2,e3,e4,e5,e6,e7,e8),e9))
+#define PV_ARRAY_1(s,e) (pv_array_append(pv_array_sized(s+1),e))
+#define PV_ARRAY_2(s,e1,e2) (pv_array_append(PV_ARRAY_1(s+1,e1),e2))
+#define PV_ARRAY_3(s,e1,e2,e3) (pv_array_append(PV_ARRAY_2(s+1,e1,e2),e3))
+#define PV_ARRAY_4(s,e1,e2,e3,e4) (pv_array_append(PV_ARRAY_3(s+1,e1,e2,e3),e4))
+#define PV_ARRAY_5(s,e1,e2,e3,e4,e5) (pv_array_append(PV_ARRAY_4(s+1,e1,e2,e3,e4),e5))
+#define PV_ARRAY_6(s,e1,e2,e3,e4,e5,e6) (pv_array_append(PV_ARRAY_5(s+1,e1,e2,e3,e4,e5),e6))
+#define PV_ARRAY_7(s,e1,e2,e3,e4,e5,e6,e7) (pv_array_append(PV_ARRAY_6(s+1,e1,e2,e3,e4,e5,e6),e7))
+#define PV_ARRAY_8(s,e1,e2,e3,e4,e5,e6,e7,e8) (pv_array_append(PV_ARRAY_7(s+1,e1,e2,e3,e4,e5,e6,e7),e8))
+#define PV_ARRAY_9(s,e1,e2,e3,e4,e5,e6,e7,e8,e9) (pv_array_append(PV_ARRAY_8(s+1,e1,e2,e3,e4,e5,e6,e7,e8),e9))
 #define PV_ARRAY_IDX(_1,_2,_3,_4,_5,_6,_7,_8,_9,NAME,...) NAME
 #define PV_ARRAY(...) \
-  PV_ARRAY_IDX(__VA_ARGS__, PV_ARRAY_9, PV_ARRAY_8, PV_ARRAY_7, PV_ARRAY_6, PV_ARRAY_5, PV_ARRAY_4, PV_ARRAY_3, PV_ARRAY_2, PV_ARRAY_1, dummy)(__VA_ARGS__)
+  PV_ARRAY_IDX(__VA_ARGS__, PV_ARRAY_9, PV_ARRAY_8, PV_ARRAY_7, PV_ARRAY_6, PV_ARRAY_5, PV_ARRAY_4, PV_ARRAY_3, PV_ARRAY_2, PV_ARRAY_1, dummy)(0,__VA_ARGS__)
 
 #endif
