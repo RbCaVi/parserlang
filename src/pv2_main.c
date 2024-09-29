9b8dffb6f70159a485e96d2407d0390e5f251f78
RbCaViTerminal <robert@robertvail.info>
2024-09-29 14:40:59 -0400

    minor changes to pv2_main

--- src/pv2_main.c
+++ src/pv2_main.c
@@ -80,14 +80,13 @@ int main(int argc, char **argv) {
 	{
 		pv val = pv_object();
 		char *s = pv_to_string(val);
-		printf("%s %s\n", s);
+		printf("%s\n", s);
 	}
 	{
 		pv val1 = pv_string("cheesy");
-		pv val2 = pv_string("burger");
-		pv val3 = pv_object();
-		char *s1 = pv_to_string(pv_object_get(pv_copy(val3), val1));
-		char *s2 = pv_to_string(val3);
+		pv val = pv_object();
+		char *s1 = pv_to_string(pv_object_get(pv_copy(val), val1));
+		char *s2 = pv_to_string(val);
 		printf("%s %s\n", s1, s2);
 	}
 	{
