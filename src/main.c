6b9db83d7257c7f9a78449de505fd881a5eb2b36
RbCaViTerminal <robert@robertvail.info>
2024-10-10 20:17:31 -0400

    remove some unnecessary lines in pl main

--- src/main.c
+++ src/main.c
@@ -14,10 +14,7 @@ int main(int, char**) {
 	pv_install();
 	pv n = PV_ARRAY(pv_number(15),pv_false(),PV_OBJECT(pv_string("key"),pv_string("value"), pv_string("key2"),pv_string("value2")));
 	pl_stack stk = pl_stack_new();
-	printf("%i\n",pv_get_refcount(n));
 	pl_dump_pv(pv_copy(n));
-	//pl_dump_pv(pv_copy(n));
-	printf("%i\n",pv_get_refcount(n));
 	stk = pl_stack_push(stk, n);
 	pl_dump_stack(stk);
 	pl_stack_unref(stk);
