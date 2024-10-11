425d33769cecec5970d8c344ea5c116a4b677f3a
RbCaViTerminal <robert@robertvail.info>
2024-10-10 20:37:21 -0400

    remove a "undefined behavior" pv_get_refcount()

--- src/pv_main.c
+++ src/pv_main.c
@@ -205,7 +205,6 @@ int main(int argc, char **argv) {
 		pv val = PV_OBJECT(pv_string("ask"), pv_string("hams"), pv_string("cheesy"), pv_string("burger"));
 		printf("%i\n", pv_get_refcount(val));
 		char *s = pv_to_string(val);
-		printf("%i\n", pv_get_refcount(val));
 		printf("%s\n", s);
 		free(s);
 	}
