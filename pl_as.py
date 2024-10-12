6275687c2d19c3e6e1b556fd9092f9b3490fd5ac
RbCaViTerminal <robert@robertvail.info>
2024-10-12 15:59:03 -0400

    add a pl "assembler"

---
+++ pl_as.py
@@ -0,0 +1,78 @@
+# pl assembler
+# converts pl assembly (.pla) to pl bytecode (.plb)
+
+import re
+import struct
+
+# output from gcc -E pl/pl_opcodes_data.h -o -
+opcodedata = '''
+OPCODE(DUP,{})
+OPCODE(POP,{})
+OPCODE(PUSHNUM,{double n;})
+OPCODE(PUSHBOOL,{int v;})
+OPCODE(SWAPN,{int n;})
+OPCODE(PUSHGLOBAL,{int i;})
+OPCODE(CALL,{int n;})
+OPCODE(RET,{})
+OPCODE(ADD,{})
+OPCODE(JUMPIF,{int target;})
+OPCODE(ARRAY,{unsigned int n;})
+OPCODE(JUMP,{int target;})
+OPCODE(APPENDA,{})
+OPCODE(ITER,{})
+OPCODE(ITERK,{})
+OPCODE(ITERV,{})
+OPCODE(ITERE,{})
+OPCODE(ITERATE,{int target;})
+'''
+
+types = {
+	"int": "i",
+	"unsigned int": "I",
+	"double": "d",
+}
+
+opcodes = {}
+for i,(opcode,members) in enumerate(re.findall('OPCODE\\(\\s*(\\S*)\\s*,\\s*(\\{.*\\})\\s*\\)', opcodedata)):
+	fmt = ''
+	for typ in re.findall('([a-zA-Z_0-9\\s]+)\\s[a-zA-Z_0-9]+;', members):
+		typ = ' '.join(typ.split())
+		fmt += types[typ]
+	opcodes[opcode] = i,fmt
+
+jumpopcodes = ["JUMP", "JUMPIF", "ITERATE"]
+
+import sys
+filein,fileout = sys.argv[1],sys.argv[2]
+
+instrs = []
+labels = {}
+blen = 0
+
+with open(filein) as f:
+	it = iter(f)
+	for line in it:
+		line = line.split('//')[0]
+		*lbls,line = line.split(':')
+		for lbl in lbls:
+			label, = lbl.split()
+			labels[label] = blen
+		if line.strip() == '':
+			continue
+		else:
+			op,*args = line.split()
+			op = op.upper()
+			blen += struct.calcsize('<I' + opcodes[op][1])
+			instrs.append((op, args, blen))
+
+bytecode = b''
+for op,args,bpos in instrs:
+	if op in jumpopcodes:
+		label, = args
+		args = [labels[label] - bpos]
+	i,fmt = opcodes[op]
+	args = [(float if typ == 'd' else int)(arg) for typ,arg in zip(fmt,args)]
+	bytecode += struct.pack('<I' + opcodes[op][1], i, *args)
+
+with open(fileout, 'wb') as f:
+	f.write(bytecode)
