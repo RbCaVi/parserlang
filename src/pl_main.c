0231858180847d7c75f6f3e12d0cd807f2d62f4b
RbCaViTerminal <robert@robertvail.info>
2024-10-12 12:23:54 -0400

    put each test in pl_main in its own scope and start writing the iteration test

--- src/pl_main.c
+++ src/pl_main.c
@@ -29,107 +29,140 @@ int main(int argc, char **argv) {
 	pl_dump_stack(stk);
 	pl_stack_unref(stk);
 
-	// call f2 twice with both inputs and make an array (should return [23, 31])
-	pl_bytecode_builder *b = pl_bytecode_new_builder();
-	pl_bytecode_builder_add(b, PUSHGLOBAL, {0});
-	pl_bytecode_builder_add(b, PUSHBOOL, {0});
-	pl_bytecode_builder_add(b, CALL, {1});
-	pl_bytecode_builder_add(b, PUSHGLOBAL, {0});
-	pl_bytecode_builder_add(b, PUSHBOOL, {1});
-	pl_bytecode_builder_add(b, CALL, {1});
-	pl_bytecode_builder_add(b, ARRAY, {2});
-	pl_bytecode_builder_add(b, PUSHBOOL, {0});
-	pl_bytecode_builder_add(b, APPENDA, {});
-	pl_bytecode_builder_add(b, RET, {});
-	pl_bytecode bytecode = pl_bytecode_from_builder(b);
+	{
+		// call f2 twice with both inputs and make an array (should return [23, 31])
+		pl_bytecode_builder *b = pl_bytecode_new_builder();
+		pl_bytecode_builder_add(b, PUSHGLOBAL, {0});
+		pl_bytecode_builder_add(b, PUSHBOOL, {0});
+		pl_bytecode_builder_add(b, CALL, {1});
+		pl_bytecode_builder_add(b, PUSHGLOBAL, {0});
+		pl_bytecode_builder_add(b, PUSHBOOL, {1});
+		pl_bytecode_builder_add(b, CALL, {1});
+		pl_bytecode_builder_add(b, ARRAY, {2});
+		pl_bytecode_builder_add(b, PUSHBOOL, {0});
+		pl_bytecode_builder_add(b, APPENDA, {});
+		pl_bytecode_builder_add(b, RET, {});
+		pl_bytecode bytecode = pl_bytecode_from_builder(b);
 
-	printf("bytecode1\n");
-	pl_bytecode_dump(bytecode);
+		printf("bytecode1\n");
+		pl_bytecode_dump(bytecode);
 
-	pv f = pl_func(bytecode);
+		pv f = pl_func(bytecode);
 
-	pl_state *pl = malloc(sizeof(pl_state));
-	pl->stack = pl_stack_new();
-	pl->globals = malloc(1 * sizeof(pv));
+		pl_state *pl = malloc(sizeof(pl_state));
+		pl->stack = pl_stack_new();
+		pl->globals = malloc(1 * sizeof(pv));
 
-	/* approximately
-	f2(b)
-		if b: goto l1
-		v1 = 8
-		v2 = 15
-		out = v1 + v2
-		v3 = true
-		if v3: goto l2
-		l1:
-		out = 31
-		l2:
-		return out
+		/* approximately
+		f2(b)
+			if b: goto l1
+			v1 = 8
+			v2 = 15
+			out = v1 + v2
+			v3 = true
+			if v3: goto l2
+			l1:
+			out = 31
+			l2:
+			return out
 
-	* simplifies to
-	f2(b)
-		if b:
-			return 31
-		else:
-			return 23
-	*/
+		* simplifies to
+		f2(b)
+			if b:
+				return 31
+			else:
+				return 23
+		*/
 
-	pl_bytecode_builder *b2 = pl_bytecode_new_builder();
-	pl_bytecode_builder_add(b2, JUMPIF, {44});
-	pl_bytecode_builder_add(b2, PUSHNUM, {8});
-	pl_bytecode_builder_add(b2, PUSHNUM, {15});
-	pl_bytecode_builder_add(b2, ADD, {});
-	pl_bytecode_builder_add(b2, PUSHBOOL, {1});
-	pl_bytecode_builder_add(b2, JUMPIF, {12});
-	pl_bytecode_builder_add(b2, PUSHNUM, {31});
-	pl_bytecode_builder_add(b2, RET, {});
-	pl_bytecode bytecode2 = pl_bytecode_from_builder(b2);
+		pl_bytecode_builder *b2 = pl_bytecode_new_builder();
+		pl_bytecode_builder_add(b2, JUMPIF, {44});
+		pl_bytecode_builder_add(b2, PUSHNUM, {8});
+		pl_bytecode_builder_add(b2, PUSHNUM, {15});
+		pl_bytecode_builder_add(b2, ADD, {});
+		pl_bytecode_builder_add(b2, PUSHBOOL, {1});
+		pl_bytecode_builder_add(b2, JUMPIF, {12});
+		pl_bytecode_builder_add(b2, PUSHNUM, {31});
+		pl_bytecode_builder_add(b2, RET, {});
+		pl_bytecode bytecode2 = pl_bytecode_from_builder(b2);
 
-	printf("bytecode2\n");
-	pl_bytecode_dump(bytecode2);
+		printf("bytecode2\n");
+		pl_bytecode_dump(bytecode2);
 
-	pl->globals[0] = pl_func(bytecode2);
+		pl->globals[0] = pl_func(bytecode2);
 
-	pv ret = pl_func_call(f, pl);
-	pl_dump_pv(ret);
+		pv ret = pl_func_call(f, pl);
+		pl_dump_pv(ret);
 
-	pl_dump_stack(pl->stack);
-	pl_stack_unref(pl->stack);
+		pl_dump_stack(pl->stack);
+		pl_stack_unref(pl->stack);
 
-	pv_free(pl->globals[0]);
-	free(pl->globals);
+		pv_free(pl->globals[0]);
+		free(pl->globals);
 
-	free(pl);
-
-	pv a = PV_ARRAY(pv_number(1), pv_number(4), pv_number(2));
-
-	pv ii = pl_iter(a);
-
-	for (int i = 0; i < 5; i++) {
-		char *s = pv_to_string(pl_iter_value(ii));
-		printf("iter %i: %s\n", i, s);
-		free(s);
-		ii = pl_iter_next(ii);
+		free(pl);
 	}
+	{
+		pv a = PV_ARRAY(pv_number(1), pv_number(4), pv_number(2));
 
-	pv_free(ii);
+		pv ii = pl_iter(a);
 
-	pv o = PV_OBJECT(pv_string("k"), pv_number(4), pv_string("k2"), pv_number(2));
+		for (int i = 0; i < 5; i++) {
+			char *s = pv_to_string(pl_iter_value(ii));
+			printf("iter %i: %s\n", i, s);
+			free(s);
+			ii = pl_iter_next(ii);
+		}
 
-	ii = pl_iter_keys(pv_copy(o));
-	pv ii2 = pl_iter_values(o);
-
-	for (int i = 0; i < 5; i++) {
-		char *s = pv_to_string(pl_iter_value(ii));
-		char *s2 = pv_to_string(pl_iter_value(ii2));
-		printf("iter %i: %s %s\n", i, s, s2);
-		free(s);
-		free(s2);
-		ii = pl_iter_next(ii);
-		ii2 = pl_iter_next(ii2);
+		pv_free(ii);
 	}
+	{
+		pv o = PV_OBJECT(pv_string("k"), pv_number(4), pv_string("k2"), pv_number(2));
 
-	pv_free(ii);
-	pv_free(ii2);
+		pv ii = pl_iter_keys(pv_copy(o));
+		pv ii2 = pl_iter_values(o);
+
+		for (int i = 0; i < 5; i++) {
+			char *s = pv_to_string(pl_iter_value(ii));
+			char *s2 = pv_to_string(pl_iter_value(ii2));
+			printf("iter %i: %s %s\n", i, s, s2);
+			free(s);
+			free(s2);
+			ii = pl_iter_next(ii);
+			ii2 = pl_iter_next(ii2);
+		}
+
+		pv_free(ii);
+		pv_free(ii2);
+	}
+	{
+		pl_bytecode_builder *b = pl_bytecode_new_builder();
+		pl_bytecode_builder_add(b, PUSHNUM, {8});
+		pl_bytecode_builder_add(b, PUSHNUM, {15});
+		pl_bytecode_builder_add(b, ARRAY, {2});
+		pl_bytecode_builder_add(b, DUP, {});
+		pl_bytecode_builder_add(b, ITERK, {});
+		pl_bytecode_builder_add(b, RET, {});
+		pl_bytecode bytecode = pl_bytecode_from_builder(b);
+
+		printf("bytecode1\n");
+		pl_bytecode_dump(bytecode);
+
+		pv f = pl_func(bytecode);
+
+		pl_state *pl = malloc(sizeof(pl_state));
+		pl->stack = pl_stack_new();
+		pl->globals = malloc(0 * sizeof(pv));
+
+		pv ret = pl_func_call(f, pl);
+		pl_dump_pv(ret);
+
+		pl_dump_stack(pl->stack);
+		pl_stack_unref(pl->stack);
+
+		free(pl->globals);
+
+		free(pl);
+	}
 
 	return 0;
 }
