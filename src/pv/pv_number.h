ddbfa4f9b9cd16e8fd23a8ee53df77a84f806b1a
RbCaVi <robert@robertvail.info>
2024-10-13 11:39:10 -0400

    split pv_number into pv_double and pv_int

--- src/pv/pv_number.h
+++ src/pv/pv_number.h
@@ -3,12 +3,16 @@
 
 #include "pv.h"
 
-extern pv_kind number_kind;
+extern pv_kind double_kind;
+extern pv_kind int_kind;
 
 void pv_number_install();
 
-pv pv_number(double);
-double pv_number_value(pv);
-int pv_is_integer(pv);
+pv pv_double(double);
+double pv_double_value(pv);
+int pv_double_is_integer(pv);
+
+pv pv_int(double);
+int pv_int_value(pv);
 
 #endif
