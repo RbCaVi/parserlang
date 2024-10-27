8eee728d4f360f954803ce68603fa074f10a51a0
RbCaViTerminal <robert@robertvail.info>
2024-10-26 21:41:58 -0400

    unify almost all the parsers into stmt

--- stmt.py
+++ stmt.py
@@ -14,32 +14,15 @@ def stmtwrap(s): # "backreference"
 def stmts(data):
 	return ['STMT','many',*data]
 
-@transform(concatstrip(strs('{'),stmts,strs('}')))
+@transform(concatstrip(strs('{'),star(noneerror(stmtwrap)),strs('}')))
 def block(data):
-	return data[1]
-
-@transform(alternate(block,stmtwrap))
-def blockstmt(data):
-	if data[0]==0:
-		return data[1]
-	else:
-		return ['STMT','many',data[1]]
+	return ['STMT','many',*data[1]]
 
 @parser
 def sym(s):
 	data,s=getSym(s)
 	if data:
 		yield data,s
-
-@transform(concatstrip(expr,strs(*[op+'=' for op in ops]),expr))
-def setop(data):
-	e1,aop,e2=data
-	return [STMT,'setop',aop,e1,e2]
-
-@transform(concatstrip(expr,strs('='),expr))
-def setstmt(data):
-	e1,_,e2=data
-	return [STMT,'set',e1,e2]
 
 typep=strip(alternate()) # no values
 
@@ -50,46 +33,59 @@ SIG='SIG'
 FN='FN'
 anytype=[TYPE,'any']
 
-@transform(concatstrip(strs('def'),optional(typep),errorafter(sym),strs('='),expr))
-def declare(data):
-	_,typ,var,__,e=data
-	return [STMT,'def',typ,var,e]
-
-@transform(concatstrip(strs('if'),expr,block))
-def ifstmt(data):
-	_,cond,__,st,___=data
-	return [STMT,'if',cond,*st]
-
-def commasep(p):
-	@transform(optional(concatstrip(p,star(concatstrip(strs(','),p)),optional(strs(',')))))
-	def commasep(data):
-		if data is None:
-			return []
-		d,others,_=data
-		return [d,*[arg for _,arg in others]]
-	return commasep
-
-@transform(concatstrip(optional(typep),optional(sym)))
-def arg(data):
+def argp(data):
 	typ,var=data
 	return [ARG,typ,var]
 
-@transform(concatstrip(sym,strs('('),commasep(arg),strs(')')))
-def funcsig(data):
-	name,_,args,_=data
-	return [SIG,name,*args]
+@transform(optional(concatstrip(concatstrip(optional(typep),optional(sym)),star(concatstrip(strs(','),concatstrip(optional(typep),optional(sym)))),optional(strs(',')))))
+def commasep(data):
+	if data is None:
+		return []
+	d,others,_=data
+	return [argp(d),*[argp(arg) for _,arg in others]]
 
-@transform(concatstrip(strs('fn'),funcsig,blockstmt))
-def func(data):
-	_,(_,name,*args),(_,_,*stmts)=data
-	return [STMT,'func',name,[SIG,*args],*stmts]
-
-@transform(expr)
-def exprstmt(data):
-	return [STMT,'expr',data]
-
-@transform(alternate(func,ifstmt,declare,setstmt,setop,exprstmt,block))
+@transform(
+	alternate(
+		concatstrip(
+			strs('fn'),concatstrip(sym,strs('('),commasep,strs(')')),alternate(block,stmtwrap)
+		),
+		concatstrip(
+			strs('if'),expr,block
+		),
+		concatstrip(
+			strs('def'),optional(typep),errorafter(sym),strs('='),expr
+		),
+		concatstrip(
+			expr,strs('='),expr
+		),
+		concatstrip(
+			expr,strs(*[op+'=' for op in ops]),expr
+		),
+		expr,
+		block
+	)
+)
 def stmt(data):
-	if data[0]==5: # block
-		return [BLOCK,*block]
-	return data[1]
+	if data[0]==0: # func
+		_,(name,_,args,_),bstmtdata=data[1]
+		if bstmtdata[0]==0:
+			(_,_,*stmts) = bstmtdata[1]
+		else:
+			*stmts = bstmtdata[1]
+		return [STMT,'func',name,[SIG,*args],*stmts]
+	if data[0]==1: # ifstmt
+		_,cond,__,st,___=data[1]
+		return [STMT,'if',cond,*st]
+	if data[0]==2: # declare
+		_,typ,var,__,e=data[1]
+		return [STMT,'def',typ,var,e]
+	if data[0]==3: # setstmt
+		e1,_,e2=data[1]
+		return [STMT,'set',e1,e2]
+	if data[0]==4: # setop
+		e1,aop,e2=data[1]
+		return [STMT,'setop',aop,e1,e2]
+	if data[0]==5: # exprstmt
+		return [STMT,'expr',data[1]]
+	if data[0]==6: # block
+		return [BLOCK,data[1]]
