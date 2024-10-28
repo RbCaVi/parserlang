ff21bdf4b318eb87b7c1dc2dd55778e9f78ec3e2
RbCaViTerminal <robert@robertvail.info>
2024-10-28 13:36:13 -0400

    plc_parsetree.c now works

--- src/plc_parsetree.c
+++ src/plc_parsetree.c
@@ -24,7 +24,16 @@ file_data readfile(char *path) {
 	return (file_data){buf, length}; // Return the buffer
 }
 
-//#include "plc_op_ids.h"
+typedef struct {
+	char *name;
+	int arity;
+} op;
+
+op ops[] = {
+#define OP(op, arity) {op, arity},
+#include "plc_op_ids.h"
+#undef OP
+};
 
 typedef enum {
 	NODE_BLOCK,
@@ -239,4 +248,88 @@ stmt parse_stmt(char *data) {
 		assert(false);
 	}
 	return out;
+}
+
+void print_indent(int indent) {
+	for (int i = 0; i < indent; i++) {
+		printf("  ");
+	}
+}
+
+void print_expr(expr e, int indent) {
+	switch (e.type) {
+	case EXPR:
+		print_indent(indent);
+		printf("EXPR %s\n", ops[e.e.id].name);
+		assert(ops[e.e.id].arity == 0 || ops[e.e.id].arity == e.e.arity);
+		for (int i = 0; i < e.e.arity; i++) {
+			print_expr(e.e.children[i], indent + 1);
+		}
+		break;
+	case NUM:
+		print_indent(indent);
+		printf("NUM %i\n", e.n.value);
+		break;
+	case SYM:
+		print_indent(indent);
+		printf("SYM %.*s\n", e.s.len, e.s.name);
+		break;
+	}
+}
+
+void print_stmt(stmt s, int indent) {
+	switch (s.type) {
+	case BLOCK:
+		print_indent(indent);
+		printf("BLOCK\n");
+		for (int i = 0; i < s.block.len; i++) {
+			print_stmt(s.block.children[i], indent + 1);
+		}
+		break;
+	case DEFFUNC:
+		print_indent(indent);
+		printf("DEFFUNC %.*s\n", s.deffunc.namelen, s.deffunc.name);
+		print_indent(indent + 1);
+		printf("SIG\n");
+		for (int i = 0; i < s.deffunc.arity; i++) {
+			print_indent(indent + 2);
+			printf("%.*s\n", s.deffunc.arglens[i], s.deffunc.args[i]);
+		}
+		print_stmt(*s.deffunc.code, indent + 1);
+		break;
+	case IF:
+		print_indent(indent);
+		printf("IF\n");
+		print_expr(*s.ifs.cond, indent + 1);
+		print_stmt(*s.ifs.code, indent + 1);
+		break;
+	case DEF:
+		print_indent(indent);
+		printf("DEF %.*s\n", s.def.namelen, s.def.name);
+		print_expr(*s.def.val, indent + 1);
+		break;
+	case RETURN:
+		print_indent(indent);
+		printf("RETURN\n");
+		print_expr(*s.ret.val, indent + 1);
+		break;
+	}
+}
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		return 1;
+	}
+
+	for (int i = 0; i < sizeof(ops) / sizeof(op); i++) {
+		printf("op %i: %s %i\n", i, ops[i].name, ops[i].arity);
+	}
+
+	file_data f = readfile(argv[1]);
+
+	stmt s = parse_stmt(f.data);
+
+	print_stmt(s, 0);
+
+	return 0;
 }
