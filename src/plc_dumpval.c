db1b91ba674015b4defc7ded4b7d0dc2d428c7cd
RbCaViTerminal <robert@robertvail.info>
2024-11-01 16:01:15 -0400

    factor out some unneeded variables

--- src/plc_dumpval.c
+++ src/plc_dumpval.c
@@ -14,20 +14,15 @@ int main(int argc, char **argv) {
 	pv_number_install();
 	pl_func_install();
 
-	pv val = PV_ARRAY(pv_double(15), pv_double(3));
-
-	pl_exe *exe = pl_exe_new();
-
-	pl_exe_add_global(exe, val);
-
 	pl_bytecode_builder *b = pl_bytecode_new_builder();
 	pl_bytecode_builder_add(b, PUSHGLOBAL, {0});
 	pl_bytecode_builder_add(b, RET, {});
 	pl_bytecode bytecode = pl_bytecode_from_builder(b);
 
-	pv f = pl_func(bytecode);
+	pl_exe *exe = pl_exe_new();
 
-	pl_exe_set_main(exe, f);
+	pl_exe_add_global(exe, PV_ARRAY(pv_double(15), pv_double(3)));
+	pl_exe_set_main(exe, pl_func(bytecode));
 
 	pl_exe_dump_file(exe, argv[1]);
 	pl_exe_free(exe);
