448ebc751dcbdf5edd0e740ab0a716fcc2aa10df
RbCaViTerminal <robert@robertvail.info>
2024-11-03 18:52:16 -0500

    added pv_to_string to pt_dump.so

--- src/pt_dump.c
+++ src/pt_dump.c
@@ -1,11 +1,16 @@
 #include "pl_exec.h"
 #include "pl_dump.h"
+#include "pv_to_string.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 pv pt_dump_pv(pl_state *pl) {
 	pv val = pl_stack_top(pl->stack);
 	printf("dumping a pv:\n");
 	pl_dump_pv(pv_copy(val));
+	char *s = pv_to_string(pv_copy(val));
+	printf("%s\n", s);
+	free(s);
 	return val;
 }
