9b6429bc6d91d8221ab3456aa57dda612f400478
RbCaViTerminal <robert@robertvail.info>
2024-12-19 21:55:22 -0500

    actually use plc_codegen_context_chain() and fix the refcounting of plc_codegen_context.globals

--- src/plc/plc_codegen.c
+++ src/plc/plc_codegen.c
@@ -13,6 +13,7 @@ struct plc_codegen_context {
 	pl_bytecode_builder *code;
 	// i'm using pv here because i'm too lazy to manage c types now
 	pv *globals; // the list of globals (for inner functions mostly)
+	int *globalrefcount;
 	pv globalmap; // object[name:global index]
 	pv stacktops; // array[stack pos]
 	int stacksize; // current size of the stack
@@ -27,15 +28,18 @@ struct plc_codegen_context {
 plc_codegen_context *plc_codegen_context_new() {
 	plc_codegen_context *out = malloc(sizeof(plc_codegen_context));
 	pv *globals = malloc(sizeof(pv));
+	int *globalrefcount = malloc(sizeof(int));
 	*globals = pv_array();
-	*out = (plc_codegen_context){pl_bytecode_new_builder(), globals, pv_object(), pv_array(), 0, pv_object(), pv_array()};
+	*globalrefcount = 1;
+	*out = (plc_codegen_context){pl_bytecode_new_builder(), globals, globalrefcount, pv_object(), pv_array(), 0, pv_object(), pv_array()};
 	return out;
 }
 
 plc_codegen_context *plc_codegen_context_chain(plc_codegen_context *c) {
 	plc_codegen_context *out = malloc(sizeof(plc_codegen_context));
 	pv_copy(*c->globals); // refcount even though this is a pointer - i will free it in plc_codegen_context_free
-	*out = (plc_codegen_context){pl_bytecode_new_builder(), c->globals, pv_copy(c->globalmap), pv_array_append(pv_copy(c->stacktops), pv_int(c->stacksize)), c->stacksize, pv_copy(c->vars)};
+	(*c->globalrefcount)++;
+	*out = (plc_codegen_context){pl_bytecode_new_builder(), c->globals, c->globalrefcount, pv_copy(c->globalmap), pv_array_append(pv_copy(c->stacktops), pv_int(c->stacksize)), c->stacksize, pv_copy(c->vars)};
 	return out;
 }
 
@@ -74,7 +78,7 @@ void plc_codegen_stmt_collect_deffunc(plc_codegen_context *c, stmt *s) {
 pl_bytecode_builder *plc_codegen_stmt(plc_codegen_context *c, stmt *s) {
 	switch (s->type) {
 		case BLOCK: {
-			plc_codegen_context *c2 = plc_codegen_context_new();
+			plc_codegen_context *c2 = plc_codegen_context_chain(c);
 			for (int i = 0; i < s->block.len; i++) {
 				plc_codegen_stmt_collect_deffunc(c2, &(s->block.children[i]));
 			}
@@ -91,7 +95,7 @@ pl_bytecode_builder *plc_codegen_stmt(plc_codegen_context *c, stmt *s) {
 		}
 		case IF: {
 			plc_codegen_expr(c, s->ifs.cond);
-			plc_codegen_context *c2 = plc_codegen_context_new();
+			plc_codegen_context *c2 = plc_codegen_context_chain(c);
 			plc_codegen_stmt(c2, s->ifs.code);
 			pl_bytecode_builder_add(c->code, JUMPIF, {8});
 			pl_bytecode_builder_add(c->code, JUMP, {pl_bytecode_builder_len(c2->code)});
@@ -145,7 +149,11 @@ pl_bytecode_builder *plc_codegen_expr(plc_codegen_context *c, expr *e) {
 void plc_codegen_context_free(plc_codegen_context *c) {
 	pl_bytecode_builder_free(c->code);
 	pv_free(*c->globals);
-	free(c->globals);
+	(*c->globalrefcount)--;
+	if (*c->globalrefcount == 0) {
+		free(c->globals);
+		free(c->globalrefcount);
+	}
 	pv_free(c->globalmap);
 	pv_free(c->stacktops);
 	pv_free(c->vars);
