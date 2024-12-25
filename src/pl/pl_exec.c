2a4da5fd540d34a598c99622d9b438c7590c717a
RbCaVi <robert@robertvail.info>
2024-12-25 10:15:23 -0500

    implement (uncompiled) HASO, APPEND, CONCAT, SETI, GETI, SET, GET (only for arrays)

--- src/pl/pl_exec.c
+++ src/pl/pl_exec.c
@@ -137,35 +137,50 @@ pv pl_next(pl_state *state) {
 				pv k = pl_stack_get(state->stack, -2);
 				pv v = pl_stack_get(state->stack, -1);
 				assert(!pv_object_has(pv_copy(o), pv_copy(k)));
-				state->stack = pl_stack_popn(state->stack, 2);
-				state->stack = pl_stack_set(state->stack, pv_object_set(o, k, v), -1);
+				state->stack = pl_stack_popn(state->stack, 3);
+				state->stack = pl_stack_push(state->stack, pv_object_set(o, k, v));
 				break;
 			}
 			opcase(DELO) {
 				pv o = pl_stack_get(state->stack, -2);
 				pv k = pl_stack_get(state->stack, -1);
-				state->stack = pl_stack_popn(state->stack, 1);
-				state->stack = pl_stack_set(state->stack, pv_object_delete(o, k), -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_object_delete(o, k));
 				break;
 			}
 			opcase(HASO) {
-				abort(); // not implemented :(
+				pv o = pl_stack_get(state->stack, -2);
+				pv k = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_bool(pv_object_has(o, k)));
 				break;
 			}
 			opcase(APPEND) {
-				abort(); // not implemented :(
+				// not implemented for strings yet :(
+				pv a = pl_stack_get(state->stack, -2);
+				pv v = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_array_append(a, v));
 				break;
 			}
 			opcase(CONCAT) {
-				abort(); // not implemented :(
+				// not implemented for strings yet :(
+				pv a1 = pl_stack_get(state->stack, -2); // steak saus
+				pv a2 = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_array_concat(a1, a2));
 				break;
 			}
 			opcase(SETI) {
-				abort(); // not implemented :(
+				pv a = pl_stack_get(state->stack, -2);
+				pv v = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_array_set(a, SETI_data.i, v));
 				break;
 			}
 			opcase(GETI) {
-				abort(); // not implemented :(
+				pv a = pl_stack_top(state->stack);
+				state->stack = pl_stack_set(state->stack, pv_array_get(a, GETI_data.i), -1);
 				break;
 			}
 			opcase(SLICE) {
@@ -205,11 +220,18 @@ pv pl_next(pl_state *state) {
 				break;
 			}
 			opcase(SET) {
-				abort(); // not implemented :(
+				pv o = pl_stack_get(state->stack, -3);
+				pv k = pl_stack_get(state->stack, -2);
+				pv v = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 3);
+				state->stack = pl_stack_push(state->stack, pv_array_set(o, k, pv_int_value(v)));
 				break;
 			}
 			opcase(GET) {
-				abort(); // not implemented :(
+				pv o = pl_stack_get(state->stack, -2);
+				pv k = pl_stack_get(state->stack, -1);
+				state->stack = pl_stack_popn(state->stack, 2);
+				state->stack = pl_stack_push(state->stack, pv_array_get(o, k));
 				break;
 			}
 			opcase(CALL) {
