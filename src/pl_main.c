613473006483f2292257a39644f7f47eab7e6008
RbCaViTerminal <robert@robertvail.info>
2025-01-26 15:56:15 -0500

    add a test for pv_iter_foreach

--- src/pl_main.c
+++ src/pl_main.c
@@ -410,6 +410,48 @@ int main(int argc, char **argv) {
 
 		pv_free(ii);
 	}
+	{
+		pl_bytecode_builder *b = pl_bytecode_new_builder();
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, PUSHINT, {8});
+		pl_bytecode_builder_add(b, MAKEARRAY, {7});
+		pl_bytecode_builder_add(b, ITER, {});
+		pl_bytecode_builder_add(b, EACH, {});
+		pl_bytecode_builder_add(b, RET, {});
+		pl_bytecode_builder_add(b, GRET, {});
+		pl_bytecode bytecode = pl_bytecode_from_builder(b);
+
+		printf("bytecode1\n");
+		pl_bytecode_dump(bytecode);
+
+		pv f = pl_func(bytecode);
+
+		pl_state *pl = pl_state_new();
+
+		pl->stack = pl_stack_push(pl->stack, f);
+		pl_state_set_call(pl, 0);
+
+		pv ii = pl_iter_gen(pl);
+
+		int i = 0;
+
+		pv_iter_foreach(ii, v) {
+			char *s = pv_to_string(v);
+			printf("iter %i: %s\n", i++, s);
+			free(s);
+		}
+
+		pv_free(ii);
+
+		//pl_dump_stack(pl->stack);
+
+		//printf("bytecode refcount at end = %i\n", pl_bytecode_getref(bytecode));
+	}
 
 	return 0;
 }
