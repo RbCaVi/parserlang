0cb330fef54598070ada81b7f9b7e50cac774494
RbCaViTerminal <robert@robertvail.info>
2025-03-19 22:53:26 -0400

    rename pv_array_data struct to pvp_array_data

--- src/pv/pv_array.c
+++ src/pv/pv_array.c
@@ -17,19 +17,19 @@ typedef struct {
   uint32_t length;
   uint32_t alloc_length;
   pv elements[];
-} pv_array_data;
+} pvp_array_data;
 
-static pv_array_data *pvp_array_get_data(pv val) {
-	pv_array_data *a = (pv_array_data*)val.data;
+static pvp_array_data *pvp_array_get_data(pv val) {
+	pvp_array_data *a = (pvp_array_data*)val.data;
 	return a;
 }
 
-static uint32_t pvp_array_length(pv_array_data *a) {
+static uint32_t pvp_array_length(pvp_array_data *a) {
 	return a->length;
 }
 
 static void pv_array_free(pv val) {
-	pv_array_data *a = pvp_array_get_data(val);
+	pvp_array_data *a = pvp_array_get_data(val);
 	uint32_t l = pvp_array_length(a);
 	for (uint32_t i = 0; i < l; i++) {
 		pv_free(a->elements[i]);
@@ -38,7 +38,7 @@ static void pv_array_free(pv val) {
 }
 
 static char *pv_array_to_string(pv val) {
-	pv_array_data *a = pvp_array_get_data(val);
+	pvp_array_data *a = pvp_array_get_data(val);
 	uint32_t l = pvp_array_length(a);
 
 	uint32_t tlen = 1 + 2 * (l == 0 ? 0 : l - 1) + 1; // "[" + ", "... + "]"
@@ -79,8 +79,8 @@ static char *pv_array_to_string(pv val) {
 }
 
 static int pv_array_equal_self(pv val1, pv val2) {
-	pv_array_data *a1 = pvp_array_get_data(val1);
-	pv_array_data *a2 = pvp_array_get_data(val2);
+	pvp_array_data *a1 = pvp_array_get_data(val1);
+	pvp_array_data *a2 = pvp_array_get_data(val2);
 	uint32_t l1 = pvp_array_length(a1);
 	uint32_t l2 = pvp_array_length(a2);
 	if (l1 != l2) {
@@ -105,27 +105,27 @@ void pv_array_install() {
 	pv_register_equal_self(array_kind, pv_array_equal_self);
 }
 
-static pv_array_data *pvp_array_alloc(size_t size) {
-	pv_array_data *a = pv_alloc(sizeof(pv_array_data) + sizeof(pv) * size);
+static pvp_array_data *pvp_array_alloc(size_t size) {
+	pvp_array_data *a = pv_alloc(sizeof(pvp_array_data) + sizeof(pv) * size);
 	a->refcnt = PV_REFCNT_INIT;
   a->alloc_length = (uint32_t)size;
   return a;
 }
 
-static pv_array_data *pvp_array_realloc(pv_array_data *ain, size_t size) {
+static pvp_array_data *pvp_array_realloc(pvp_array_data *ain, size_t size) {
 	assert(size >= ain->length);
-	pv_array_data *a;
+	pvp_array_data *a;
 	if (pvp_refcnt_unshared(&(ain->refcnt))) {
 		// there is only one copy of this pv, so i can rebuild it with realloc
 		// if (size <= ain->alloc_length) {
 		// 	// don't need to reallocate if it would shorten the allocation
 		// 	return ain;
 		// }
-		a = pv_realloc(ain, sizeof(pv_array_data) + sizeof(pv) * size);
+		a = pv_realloc(ain, sizeof(pvp_array_data) + sizeof(pv) * size);
 	} else {
 		// there is more then one copy of this pv, so i have to 
-		a = pv_alloc(sizeof(pv_array_data) + sizeof(pv) * size);
-		memcpy(a, ain, sizeof(pv_array_data) + sizeof(pv) * ain->length);
+		a = pv_alloc(sizeof(pvp_array_data) + sizeof(pv) * size);
+		memcpy(a, ain, sizeof(pvp_array_data) + sizeof(pv) * ain->length);
 		for (uint32_t i = 0; i < pvp_array_length(a); i++) {
 			pv_copy(a->elements[i]);
 		}
@@ -138,7 +138,7 @@ static pv_array_data *pvp_array_realloc(pv_array_data *ain, size_t size) {
 
 pv pv_array(void) {
 	// slightly simpler than pv_string
-	pv_array_data *a = pvp_array_alloc(16);
+	pvp_array_data *a = pvp_array_alloc(16);
   a->length = 0;
   pv val = {array_kind, PV_FLAG_ALLOCATED, &(a->refcnt)};
   return val;
@@ -146,7 +146,7 @@ pv pv_array(void) {
 
 pv pv_array_sized(uint32_t size) {
 	// slightly simpler than pv_string
-	pv_array_data *a = pvp_array_alloc(size);
+	pvp_array_data *a = pvp_array_alloc(size);
   a->length = 0;
   pv val = {array_kind, PV_FLAG_ALLOCATED, &(a->refcnt)};
   return val;
@@ -170,7 +170,7 @@ int pvp_array_wrap_idx(int i, uint32_t l) {
 
 pv pv_array_get(pv val, int i) {
 	assert(val.kind == array_kind);
-	pv_array_data *a = pvp_array_get_data(val);
+	pvp_array_data *a = pvp_array_get_data(val);
 	i = pvp_array_wrap_idx(i, pvp_array_length(a));
 	pv out = pv_copy(a->elements[i]);
 	pv_free(val);
@@ -180,10 +180,10 @@ pv pv_array_get(pv val, int i) {
 // slightly different from jq (jq extends arrays with null on out of bounds write)
 pv pv_array_set(pv val, int i, pv cell) {
 	assert(val.kind == array_kind);
-	pv_array_data *a = pvp_array_get_data(val);
+	pvp_array_data *a = pvp_array_get_data(val);
 	uint32_t l = pvp_array_length(a);
 	i = pvp_array_wrap_idx(i, l);
-	pv_array_data *newa = pvp_array_realloc(a, a->alloc_length);
+	pvp_array_data *newa = pvp_array_realloc(a, a->alloc_length);
 	pv_free(newa->elements[i]);
 	newa->elements[i] = cell;
   pv newval = {array_kind, PV_FLAG_ALLOCATED, &(newa->refcnt)};
@@ -192,9 +192,9 @@ pv pv_array_set(pv val, int i, pv cell) {
 
 pv pv_array_append(pv val, pv cell) {
 	assert(val.kind == array_kind);
-	pv_array_data *a = pvp_array_get_data(val);
+	pvp_array_data *a = pvp_array_get_data(val);
 	uint32_t l = pvp_array_length(a);
-	pv_array_data *newa = pvp_array_realloc(a, max(a->alloc_length, l + 1));
+	pvp_array_data *newa = pvp_array_realloc(a, max(a->alloc_length, l + 1));
 	newa->length = l + 1;
 	newa->elements[l] = cell;
   pv newval = {array_kind, PV_FLAG_ALLOCATED, &(newa->refcnt)};
@@ -204,11 +204,11 @@ pv pv_array_append(pv val, pv cell) {
 pv pv_array_concat(pv val1, pv val2) {
 	assert(val1.kind == array_kind);
 	assert(val2.kind == array_kind);
-	pv_array_data *a1 = pvp_array_get_data(val1);
-	pv_array_data *a2 = pvp_array_get_data(val2);
+	pvp_array_data *a1 = pvp_array_get_data(val1);
+	pvp_array_data *a2 = pvp_array_get_data(val2);
 	uint32_t l1 = pvp_array_length(a1);
 	uint32_t l2 = pvp_array_length(a2);
-	pv_array_data *a = pvp_array_realloc(a1, max(a1->alloc_length, l1 + l2));
+	pvp_array_data *a = pvp_array_realloc(a1, max(a1->alloc_length, l1 + l2));
 	a->length = l1 + l2;
 	for (uint32_t i = 0; i < l2; i++) {
 		a->elements[l1 + i] = pv_copy(a2->elements[i]);
