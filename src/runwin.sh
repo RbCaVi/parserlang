b339d5f96d13e72ef1672405f2738ac6d935070c
RbCaVijjp <robert@vail-family.com>
2025-04-10 10:04:33 -0400

    pl_builtins is linked into pl.so

--- src/runwin.sh
+++ src/runwin.sh
@@ -1,6 +1,7 @@
 prgm="$1"
 rm -f ../${prgm}.plp &&
 make -C .. ${prgm}.plp &&
+make pv.so pl.so plc_dumptree plc_compile pl_run &&
 LD_LIBRARY_PATH=. ./plc_dumptree.exe ../${prgm}.plp &&
 LD_LIBRARY_PATH=. ./plc_compile.exe ../${prgm}.plp ${prgm}.plc &&
 LD_LIBRARY_PATH=. ./pl_run.exe ${prgm}.plc
